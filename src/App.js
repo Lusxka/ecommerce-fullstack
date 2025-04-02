77c504ec8a5a549b5f9bc89a2ced343fbd764b10
Lusxka <you@example.com>
2025-04-02 01:13:43 -0300

    Dependências bootstrap e configuração de ambiente

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 class="text-danger">Promoforia</h1>
+    </>
+  );
+}
+
+export default App;
